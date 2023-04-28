51890e6292ac33f37d79e5e1b8205a475eca6631
Diego Rubin <diegorubin@ciandt.com>
2023-04-28 07:32:10 -0300

    updating deps

--- lifeguard_simple_dashboard/__init__.py
+++ lifeguard_simple_dashboard/__init__.py
@@ -8,7 +8,7 @@ from lifeguard_simple_dashboard.settings import LIFEGUARD_DASHBOARD_PREFIX_PATH
 class LifeguardSimpleDashboardPlugin:
     def __init__(self, lifeguard_context):
         self.lifeguard_context = lifeguard_context
-        register_custom_controller("{}/css/<path:path>".format(LIFEGUARD_DASHBOARD_PREFIX_PATH), send_css, {"methods": ["GET"]})
+        register_custom_controller("{}/simple-dashboard-css/<path:path>".format(LIFEGUARD_DASHBOARD_PREFIX_PATH), send_css, {"methods": ["GET"]})
         register_custom_controller("{}/".format(LIFEGUARD_DASHBOARD_PREFIX_PATH), index, {"methods": ["GET"]})
 
 def init(lifeguard_context):
