d851456789d323cd1a11119d8ece1165bbb8c431
Diego Rubin <contact@diegorubin.dev>
2023-05-24 09:34:20 -0300

    updating deps

--- lifeguard_simple_dashboard/controllers/index_controller.py
+++ lifeguard_simple_dashboard/controllers/index_controller.py
@@ -6,7 +6,10 @@ from lifeguard.controllers import Response
 from lifeguard.logger import lifeguard_logger as logger
 from lifeguard.repositories import ValidationRepository
 
+from lifeguard_simple_dashboard.settings import LIFEGUARD_DASHBOARD_PREFIX_PATH
+
 SEARCHPATH = str(join(pathlib.Path(__file__).parent.parent.absolute(), "templates"))
+
 
 def render_template(template, data=None):
     response = Response()
@@ -15,10 +18,12 @@ def render_template(template, data=None):
     response.data = data
     return response
 
+
 def send_status(status):
     response = Response()
     response.status = status
     return response
+
 
 def send_file(path, content_type):
     absolute_path = pathlib.Path(__file__).parent.parent.absolute()
@@ -28,17 +33,29 @@ def send_file(path, content_type):
         response.content_type = content_type
         return response
 
-def send_css(path):
+
+def dashboard_send_css(path):
     try:
         logger.info("returning css %s", path)
         return send_file("css/{}".format(path), "text/css")
     except:
         return send_status(404)
 
+
 def index():
     try:
         logger.info("rendering index")
         validations = ValidationRepository().fetch_all_validation_results()
-        return render_template("dashboard.html", {"validations": validations})
+        return render_template(
+            "dashboard.html",
+            {
+                "validations": validations,
+                "LIFEGUARD_DASHBOARD_PREFIX_PATH": LIFEGUARD_DASHBOARD_PREFIX_PATH,
+            },
+        )
     except Exception as error:
-        logger.error("error on render dashboard index: %s", error, extra={"traceback":traceback.format_exc()})
+        logger.error(
+            "error on render dashboard index: %s",
+            error,
+            extra={"traceback": traceback.format_exc()},
+        )
